404dad462c62aac68b7f94abdd3cdd778c061f43
dede herdiana <dedeherdiana71@gmail.com>
2019-10-01 17:09:11 +0700

    initial commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class='container'>
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/about">About</router-link>
+                    </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'navigation'
+}
+</script>
